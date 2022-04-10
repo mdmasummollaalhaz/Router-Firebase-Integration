f208eef68adce4831fa57c630e931768f09f7b36
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-11 04:47:38 +0600

    Firebase hooks

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,6 +5,9 @@ const Login = () => {
     return (
         <div>
             <h3>Please, Login</h3>
+            <div style={{margin:'20px'}}>
+                <button>Google Sing In</button>
+            </div>
             <form action="">
                 <input type="text" placeholder='Your name' name="" id="" />
                 <br />
