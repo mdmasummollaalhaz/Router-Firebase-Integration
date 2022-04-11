2f9dd01b3f0b9b4f8e6a3eb3f3684c6828309cb6
Masum Mollo <freelancermasummolla@gmail.com>
2022-04-12 05:29:01 +0600

    google sign In & Out & display name

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,16 +1,40 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
+import app from '../firebase.init'
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const useFirebase = () =>{
     const [user, setUser] = useState({});
 
-    useEffect( () =>{
+    const signInWithGoogle = () =>{
+        // console.log('Sign in soon');
+        signInWithPopup(auth, googleProvider)
+            .then(result =>{
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+            })
+            .catch()
+    }
 
+    const handleSignOut = () =>{
+        signOut(auth)
+            .then(() =>{
+
+            })
+            .catch()
+    }
+
+    useEffect( () =>{
+        onAuthStateChanged(auth, user =>{
+            setUser(user);
+        })
     }, []);
 
-    const signInWithGoogle = () =>{
-        console.log('Sign in soon');
-    }
-    return [user, setUser];
+    // return [user, setUser];
+    return{user, signInWithGoogle, handleSignOut}
 }
 
 export default useFirebase;
